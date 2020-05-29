cfec94281c3fde160e7203b1797dc364f5708104
weivenotto <weivenotto@gmail.com>
2020-05-29 01:08:09 +0000

    server e client finalizado

--- Client.c
+++ Client.c
@@ -1,52 +1,55 @@
-#include <arpa/inet.h> //inet_addr
 #include <stdio.h>
-#include <string.h> //strlen
-#include <sys/socket.h>
+#include <string.h>    //strlen
 #include <unistd.h> // close
+#include <sys/socket.h> 
+#include <arpa/inet.h>    //inet_addr
 
-int main(int argc, char *argv[]) {
-  int socketDesc;
-  struct sockaddr_in server;
-  char *message, server_reply[2000];
+int main(int argc , char *argv[])
+{
+    int socket_desc;
+    struct sockaddr_in server;
+    char *message , server_reply[2000];
 
-  // AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP
-  socketDesc = socket(AF_INET, SOCK_STREAM, 0);
+    // Criar socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (socket_desc == -1)
+    {
+        printf("Impossível criar socket");
+    }
+    
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
 
-  if (socketDesc == -1) {
-    printf("Não foi possivel criar o socket.\n");
-    return 1;
-  }
+    //Conectar ao socket remoto
+    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("connect error");
+        return 1;
+    }
+    
+    puts("Connected");
+    
+    //Envia dados
+    message = "Hello, World!\n";
+    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    {
+        puts("Falha ao enviar");
+        return 1;
+    }
+    puts("Dados enviados\n");
+    
+    //Recebe resposta do servidor
+    if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+    {
+        puts("Falha ao receber.");
+        return 1;
+    }
+    puts("Resposta recebida\n");
+    puts(server_reply);
 
-  server.sin_addr.s_addr = inet_addr("172.217.28.132");
-  server.sin_family = AF_INET;
-  server.sin_port = htons(80);
+    close(socket_desc); // termina o socket
 
-  if (connect(socketDesc, (struct sockaddr *)&server, sizeof(server)) == -1) {
-    printf("Não foi possivel conectar-se.\n");
-    return 1;
-  }
-
-  printf("Connected.\n");
-
-  // Envia dados
-  message = "GET / HTTP/1.1\r\n\r\n";
-  if (send(socketDesc, message, strlen(message), 0) < 0) {
-    printf("Falha ao enviar.\n");
-    return 1;
-  }
-
-  printf("Dados enviados.\n");
-
-  // Recebe resposta do servidor
-  if (recv(socketDesc, server_reply, 2000, 0) < 0) {
-    printf("Falha ao receber.\n");
-    return 1;
-  }
-
-  printf("Resposta recebida.\n");
-  puts(server_reply);
-
-  close(socketDesc); // termina o socket
-
-  return 0;
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,28 +1,54 @@
-#include <arpa/inet.h> //inet_addr
 #include <stdio.h>
-#include <string.h> //strlen
+#include <string.h>    //strlen
 #include <sys/socket.h>
-#include <unistd.h> // close
+#include <arpa/inet.h>    //inet_addr
+#include <unistd.h>    //write
 
-int main(int argc, char *argv[]) {
-  int socketDesc;
-  struct sockaddr_in server;
-
-  socketDesc = socket(AF_INET, SOCK_STREAM, 0);
-
-  if (socketDesc == -1) {
-    printf("Não foi possivel criar o socket.\n");
-    return 1;
-  }
-
+int main(int argc , char *argv[])
+{
+    int socket_desc , new_socket , c;
+    struct sockaddr_in server , client;
+    char *message;
+    
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    
+    //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY; /*escute em qualquer IP da maquina */
-    server.sin_port = htons(8888);
-
-    if(bind(socketDesc, (struct sockaddr *) &server, sizeof(server)) < 0){
-        printf("Erro ao fazer o bind\n");
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+    
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("bind failed");
+    }
+    puts("bind done");
+    
+    //Listen
+    listen(socket_desc , 3);
+    
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        puts("Connection accepted");
+        
+        //Reply to the client
+        message = "Hello Client , I have received your connection. But I have to go now, bye\n";
+        write(new_socket , message , strlen(message));
+    }
+    
+    if (new_socket<0)
+    {
+        perror("accept failed");
         return 1;
     }
-    printf("bind efetuado.\n");
+
     return 0;
 }
